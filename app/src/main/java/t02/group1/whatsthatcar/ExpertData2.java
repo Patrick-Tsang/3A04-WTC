527508f3d3fd50555a454cc4760762f8f79d2f48
Patrick Tsang <tsangp3@mcmaster.ca>
2018-04-02 11:11:57 -0400

    Initial Commit

---
+++ app/src/main/java/t02/group1/whatsthatcar/ExpertData2.java
@@ -0,0 +1,8 @@
+package t02.group1.whatsthatcar;
+
+/**
+ * Created by Patrick on 2018-04-02.
+ */
+
+public class ExpertData2 {
+}
---
+++ app/src/main/java/t02/group1/whatsthatcar/SearchView.java
@@ -0,0 +1,9 @@
+package t02.group1.whatsthatcar;
+
+/**
+ * Created by Patrick on 2018-04-02.
+ */
+
+public class SearchView {
+
+}
