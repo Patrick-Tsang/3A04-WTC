4af9802979ce8e92bf859c693e43401435c19a03
Stanliest <iamstanley2014@gmail.com>
2018-04-03 10:44:55 -0400

    update code in expertDatas

--- app/src/main/java/t02/group1/whatsthatcar/ExpertData1.java
+++ app/src/main/java/t02/group1/whatsthatcar/ExpertData1.java
@@ -1,31 +1,31 @@
 package t02.group1.whatsthatcar;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
- * Created by Patrick on 2018-04-02.
+ * This is the ExpertData class for "make"
+ * @author  Stanley Liu
+ * @date    03/04/2018
  */
 
-// This is the expert data for the vehicle manufacturer
 public class ExpertData1 {
 
-    HashMap<Integer, String> dataset = new HashMap<>(); // not sure when to use this yet
+    /**
+     * The search method read through the data file and find the make that equals to the input
+     * @param search
+     * @return return a list of integers that corresponds to the index of the row
+     * @throws IOException
+     */
+    public static List<Integer> search(String search) throws IOException {
 
-    // do we need the information for the whole row to be returned, or just a list of row number
-    public List<Integer> search(String search) throws IOException {
-
-        List<Integer> results = new ArrayList<>();
-        String file = "data1.csv";
+        List<Integer> results1 = new ArrayList<>();
+        String file = "data1.csv"; // file that contains only info of "make"
         String line = "";
         List<String> data = new ArrayList<>();
-        List<String> filteredData = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         while ((line = br.readLine()) != null) {
@@ -35,13 +35,20 @@ public class ExpertData1 {
         br.close();
 
         for (int i = 1; i < data.size(); i++) { // i start with 1 here to skip the headings
-            if (data.get(i).split(",")[0].equals(search)) {
-                results.add(i); // the index of the row
+            if (data.get(i).equals(search)) {
+                results1.add(i); // the index of the row
             }
         }
 
-        return results;
+        return results1;
     }
 
+
+//    public static void main(String args[]) throws IOException {
+//        List<Integer> list = search("BMW");
+//        for (int i=0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//    }
 
 }
