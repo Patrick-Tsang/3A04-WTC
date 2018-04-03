19871f956fd7f2d067ab9b8eb3fcba045ccc392c
Stanliest <iamstanley2014@gmail.com>
2018-04-02 22:52:01 -0400

    completed method search, have not implemented hashmap yet

--- app/src/main/java/t02/group1/whatsthatcar/ExpertData1.java
+++ app/src/main/java/t02/group1/whatsthatcar/ExpertData1.java
@@ -1,27 +1,46 @@
 package t02.group1.whatsthatcar;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Patrick on 2018-04-02.
  */
 
+// This is the expert data for the vehicle manufacturer
 public class ExpertData1 {
 
-    List<Map<String, String>> dataset;
+    HashMap<Integer, String> dataset = new HashMap<>(); // not sure when to use this yet
 
-    public ExpertData1(){
-         dataset = new ArrayList<>();
-    }
+    // do we need the information for the whole row to be returned, or just a list of row number
+    public List<Integer> search(String search) throws IOException {
 
-
-
-
-    public List<Integer> search(String search) {
         List<Integer> results = new ArrayList<>();
+        String file = "data.csv";
+        String line = "";
+        List<String> data = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while ((line = br.readLine()) != null) {
+            data.add(line);
+        }
+
+        br.close();
+
+        for (int i = 1; i < data.size(); i++) { // i start with 1 here to skip the headings
+            if (data.get(i).split(",")[0].equals(search)) {
+                results.add(i); // the index of the row
+            }
+        }
+
         return results;
     }
+
+
 }
