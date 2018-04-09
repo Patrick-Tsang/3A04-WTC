366795f7897e2a1766786f522ddda73f6a5665a9
zhzhzi <zhang325200@gmail.com>
2018-04-08 23:38:18 -0400

    Update HelpView.java

--- app/src/main/java/t02/group1/whatsthatcar/HelpView.java
+++ app/src/main/java/t02/group1/whatsthatcar/HelpView.java
@@ -1,7 +1,12 @@
 package t02.group1.whatsthatcar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HelpView extends AppCompatActivity {
     public String displayHelp(){
@@ -12,8 +17,46 @@ public class HelpView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_view);
+
+        findViewById(R.id.btnSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelp("Search Help", HelpData.getSearchHelp());
+            }
+        });
+
+        findViewById(R.id.btnHistory).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelp("History Help", HelpData.getHistoryHelp());
+            }
+        });
+        findViewById(R.id.btnGeneral).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelp("Ceneral Help", HelpData.getGeneralHelp());
+            }
+        });
     }
 
+    private void showHelp(String title,String help)
+    {
+        AlertDialog.Builder localBuilder = new AlertDialog.Builder(HelpView.this);
+        localBuilder.setTitle(title);
+        localBuilder.setIcon(R.mipmap.ic_launcher);
+        localBuilder.setMessage(help);
+        localBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt)
+            {
+
+            }
+        });
+
+        localBuilder.setCancelable(false).create();
+
+        localBuilder.show();
+    }
     public void closeHelp(){
 
     }
