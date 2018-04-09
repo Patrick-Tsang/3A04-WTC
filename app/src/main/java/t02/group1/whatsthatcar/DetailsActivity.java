88c3a834d0e44d3f9bb12d5927b805deacc817b3
zhzhzi <zhang325200@gmail.com>
2018-04-08 23:36:34 -0400

    Update DetailsActivity.java

--- app/src/main/java/t02/group1/whatsthatcar/DetailsActivity.java
+++ app/src/main/java/t02/group1/whatsthatcar/DetailsActivity.java
@@ -1,4 +1,13 @@
 package t02.group1.whatsthatcar;
 
-public class DetailsActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
 }
